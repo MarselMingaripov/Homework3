97788d159f983e89e5f6b896e2d7b714e790ce2e
MarselMingaripov <bard1812@gmail.com>
2022-11-29 12:12:31 +0300

    выполнил задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int var1 =10;
+        System.out.println("Значение переменной var1 с типом int равно " + var1);
+        byte var2 = 15;
+        System.out.println("Значение переменной var2 с типом byte равно " + var1);
+        short var3 = 20;
+        System.out.println("Значение переменной var3 с типом short равно " + var1);
+        long var4 = 25;
+        System.out.println("Значение переменной var4 с типом long равно " + var1);
+        float var5 = 30.5F;
+        System.out.println("Значение переменной var5 с типом float равно " + var1);
+        double var6 = 35.5;
+        System.out.println("Значение переменной var6 с типом double равно " + var1);
     }
 }
